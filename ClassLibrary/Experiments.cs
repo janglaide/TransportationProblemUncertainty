d7d0e70b8a56c905b998b0be027b7db71fe19519
suodbons <rivanota@gmail.com>
2020-12-06 23:33:11 +0200

    minor experiments stuff

---
+++ ClassLibrary/Experiments.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ClassLibrary
+{
+    public class Experiments
+    {
+        private Generator generator = new Generator();
+        public List<double> GenerateMatrix(int size, string distr, double lambda)
+        {
+            List<double> matrix = new List<double>();
+            for (int i = 0; i < size; i++)
+            {
+                for(int j = 0; j < size; j++)
+                {
+                    matrix.Add(generator.GetValue(distr, lambda));
+                }
+            }
+            return matrix;
+        }
+        public List<double> GenerateMatrix(int size, string distr, int mean, int dev)
+        {
+            List<double> matrix = new List<double>();
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    matrix.Add(generator.GetValue(distr, mean, dev));
+                }
+            }
+            return matrix;
+        }
+        public void ChangeMatrix(ref List<double> matrix, double percent, string distr)
+        {
+            int size = (int)Math.Round(Math.Sqrt(matrix.Count));
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    double e = matrix[i * size + j] * (percent / 100);
+                    double factE;
+                    if(distr == "exp")
+                    {
+                        factE = generator.GetValue(distr, e);
+                    }
+                    else
+                    {
+                        factE = generator.GetValue(distr, 0, e);
+                    }
+                    matrix[i * size + j] += factE;
+                }
+            }
+        }
+        public double IWantToDieSorryIWillFixThisNameForSearchMeanPercentMethod(int size, string distr)
+        {
+            double average = 0;
+            double diff = double.MaxValue;
+            int runAmount = 0;
+            while (diff > 0.1)
+            {
+                diff = runAmount != 0 ? average / runAmount : 0;                
+                average += SomeMagicMethodForExactPercentOfChange(size, distr);
+                runAmount++;
+                diff = average / runAmount - diff;
+            }
+        }
+    }
+}
---
+++ ClassLibrary/Generator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class Generator
+    {
+        private static Random rand = new Random();
+        public double GetValue(string distr, double lambda)
+        {
+            return Exponential(lambda);
+        }
+        public double GetValue(string distr, double mean, double dev)
+        {
+            switch (distr)
+            {
+                case "norm":
+                    return Normal(mean, dev);
+                case "unif":
+                    return Uniform(mean - dev, mean + dev);
+            }
+            return double.MinValue; 
+        }
+        public double Exponential(double lambda)
+        {
+            return -Math.Log(rand.NextDouble()) / lambda;
+        }
+        public double Normal(double mean, double deviation)
+        {
+            double mu = 0;
+            for (int j = 0; j < 12; j++)
+            {
+                mu += rand.NextDouble();
+            }
+            mu -= 6;
+            return deviation * mu + mean;
+        }
+        public double Uniform(double min, double max)
+        {
+            return rand.NextDouble()*(max - min) + min;
+        }
+    }
+}
